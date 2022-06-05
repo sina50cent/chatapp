8d87c9ec2b567701ae167b8164e0c158728905f4
sina <sina.abouhamzeh@gmail.com>
2022-06-05 17:31:09 +0430

    add model and view to chatapp and testing view

--- user_control/models.py
+++ user_control/models.py
@@ -1,6 +1,6 @@
 from django.db import models
 from django.contrib.auth.models import AbstractBaseUser, PermissionsMixin, BaseUserManager
-from message_control.models import GenericFileUpload
+# from message_control.models import GenericFileUpload
 from django.utils import timezone
 
 
@@ -26,7 +26,7 @@ class CustomUserManager(BaseUserManager):
         if extra_fields.get("is_superuser") is not True:
             raise ValueError("Superuser must have is_superuser=True.")
 
-        return self._create_user(username, password, **extra_fields)
+        return self.create_user(username, password, **extra_fields)
 
 
 class CustomUser(AbstractBaseUser, PermissionsMixin):
@@ -41,7 +41,10 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
 
     USERNAME_FIELD = "username"
     objects = CustomUserManager()
-
+    """
+        this objects is for querying ,
+        A Manager is the interface through which database query operations are provided to Django models
+    """
     def __str__(self):
         return self.username
 
@@ -49,37 +52,37 @@ class CustomUser(AbstractBaseUser, PermissionsMixin):
         ordering = ("created_at",)
 
 
-class UserProfile(models.Model):
-    user = models.OneToOneField(
-        CustomUser, related_name="user_profile", on_delete=models.CASCADE)
-    first_name = models.CharField(max_length=100)
-    last_name = models.CharField(max_length=100)
-    caption = models.CharField(max_length=250)
-    about = models.TextField()
-    profile_picture = models.ForeignKey(
-        GenericFileUpload, related_name="user_image", on_delete=models.SET_NULL, null=True)
-    created_at = models.DateTimeField(auto_now_add=True)
-    updated_at = models.DateTimeField(auto_now=True)
-
-    def __str__(self):
-        return self.user.username
-
-    class Meta:
-        ordering = ("created_at",)
-
-
-class Favorite(models.Model):
-    user = models.OneToOneField(CustomUser, related_name="user_favorites", on_delete=models.CASCADE)
-    favorite = models.ManyToManyField(CustomUser, related_name="user_favoured")
-    created_at = models.DateTimeField(auto_now_add=True)
-
-    def __str__(self):
-        return f"{self.user.username}"
-
-    class Meta:
-        ordering = ("created_at",)
-
-
+# class UserProfile(models.Model):
+#     user = models.OneToOneField(
+#         CustomUser, related_name="user_profile", on_delete=models.CASCADE)
+#     first_name = models.CharField(max_length=100)
+#     last_name = models.CharField(max_length=100)
+#     caption = models.CharField(max_length=250)
+#     about = models.TextField()
+#     profile_picture = models.ForeignKey(
+#         GenericFileUpload, related_name="user_image", on_delete=models.SET_NULL, null=True)
+#     created_at = models.DateTimeField(auto_now_add=True)
+#     updated_at = models.DateTimeField(auto_now=True)
+#
+#     def __str__(self):
+#         return self.user.username
+#
+#     class Meta:
+#         ordering = ("created_at",)
+#
+#
+# class Favorite(models.Model):
+#     user = models.OneToOneField(CustomUser, related_name="user_favorites", on_delete=models.CASCADE)
+#     favorite = models.ManyToManyField(CustomUser, related_name="user_favoured")
+#     created_at = models.DateTimeField(auto_now_add=True)
+#
+#     def __str__(self):
+#         return f"{self.user.username}"
+#
+#     class Meta:
+#         ordering = ("created_at",)
+#
+#
 class Jwt(models.Model):
     user = models.OneToOneField(
         CustomUser, related_name="login_user", on_delete=models.CASCADE)
--- user_control/tests.py
+++ user_control/tests.py
@@ -1,3 +1,39 @@
-from django.test import TestCase
+from rest_framework.test import APITestCase
+from .views import get_random,get_access_token,get_refresh_token
 
-# Create your tests here.
+
+class TestGenericFunctions(APITestCase):
+    
+    def test_get_random(self):
+        rand1 = get_random(10)
+        rand2 = get_random(10)
+        rand3 = get_random(15)
+
+        # check that we are getting a result
+        self.assertTrue(rand1)
+        
+        # check that rand1 is not equal to rand2        
+        self.assertNotEqual(rand1, rand2)
+        
+        # check that the length if result is what is excepted
+        self.assertEqual(len(rand1),10)
+#        self.assertEqual(len(rand1),11)
+        self.assertEqual(len(rand3),15)
+        
+    
+    def test_get_access_token(self):
+        payload = {
+            'id': 1 
+        }
+        
+        token = get_access_token(payload)
+        
+        # check that we obtained a result
+        self.assertTrue(token)
+        
+        
+    def test_get_refresh_token(self):
+        token = get_refresh_token()
+        
+        # check that we obtained a result
+        self.assertTrue(token)
